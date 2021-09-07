d023a362f0f8ca1ed0cdfb5fc7c9b601d92c7798
kt-postnikova <kt-postnikova@yandex.ru>
2021-09-07 16:51:48 +0300

    Подключение mongoose

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/moviesdb', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.listen(PORT);
