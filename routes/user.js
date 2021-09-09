d397bdcf36e2b7102287357051439b758ff2879b
kt-postnikova <kt-postnikova@yandex.ru>
2021-09-09 12:53:00 +0300

    Созданы руты для пользователя

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,14 @@
 const usersRouter = require('express').Router();
-const { getUserInfo, updateUserProfile } = require('../controllers/users');
+const { getUserInfo, updateUserInfo, deleteUser, getUsers } = require('../controllers/users');
+const { userInfoValidator } = require('../middlewares/validation');
+const auth = require('../middlewares/auth');
 
-usersRouter.get('/users/me', getUserInfo);
-usersRouter.patch('/users/me', updateUserProfile);
+usersRouter.delete('/users/:id', deleteUser);
+usersRouter.get('/users', getUsers);
+
+usersRouter.get('/users/me', auth, userInfoValidator, getUserInfo);
+usersRouter.patch('/users/me', auth, userInfoValidator, updateUserInfo);
+
+
 
 module.exports = usersRouter;
