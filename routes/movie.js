c86e81f0b3c2eed22b225ec9d2acf51a5f836aed
kt-postnikova <kt-postnikova@yandex.ru>
2021-09-09 14:07:53 +0300

    Добавлена валидация создания фильма

--- routes/movie.js
+++ routes/movie.js
@@ -1,9 +1,10 @@
 const moviesRouter = require('express').Router();
 const { getMovies, createMovie, deleteMovie } = require('../controllers/movies');
 const auth = require('../middlewares/auth');
+const { createMovieValidator } = require('../middlewares/validation');
 
 moviesRouter.get('/movies', auth, getMovies);
-moviesRouter.post('/movies', auth, createMovie);
+moviesRouter.post('/movies', auth, createMovieValidator, createMovie);
 moviesRouter.delete('/movies/:id', auth, deleteMovie);
 
 module.exports = moviesRouter;
