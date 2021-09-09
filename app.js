8a9dba9479f395025ac2e916443ed7462c0fe1ae
kt-postnikova <kt-postnikova@yandex.ru>
2021-09-09 12:52:45 +0300

    Создан исходный файл app.js

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+const userRouter = require('./routes/user');
+const moviesRouter = require('./routes/movie');
+const { registration, login } = require('./controllers/users');
+// const auth = require('./middlewares/auth');
+const { registrationValidator, loginValidator } = require('./middlewares/validation');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const app = express();
+
+const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(requestLogger);
+
+app.post('/signin', loginValidator, login);
+app.post('/signup', registrationValidator, registration);
+
+// app.use(auth);
+app.use('/', userRouter);
+app.use('/', moviesRouter);
+
+app.use(errors());
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res.status(statusCode).send({
+    message: statusCode === 500
+      ? 'На сервере произошла ошибка'
+      : message
+  });
+});
+
+app.listen(PORT);
