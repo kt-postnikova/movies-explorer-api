eea680a34b371ada5da62950d7a1c3d92ac3ef0d
kt-postnikova <kt-postnikova@yandex.ru>
2021-09-10 12:16:37 +0300

    Настроен rate limiter

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 require('dotenv').config();
+const rateLimit = require('express-rate-limit');
 const userRouter = require('./routes/user');
 const moviesRouter = require('./routes/movie');
 const { registration, login } = require('./controllers/users');
@@ -34,5 +35,12 @@ app.use((err, req, res) => {
 
   res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
 });
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
 
 app.listen(PORT);
