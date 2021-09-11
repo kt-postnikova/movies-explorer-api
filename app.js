67ceffe946bac61bc8113c03c6bc2c4898fec51a
kt-postnikova <kt-postnikova@yandex.ru>
2021-09-11 13:08:33 +0300

    Исправлено: порядок подключения логгеров и лимитера + адрес прописан статически и не зависит от режима запуска приложения

--- app.js
+++ app.js
@@ -7,34 +7,21 @@ const rateLimit = require('express-rate-limit');
 const userRouter = require('./routes/user');
 const moviesRouter = require('./routes/movie');
 const entranceRouter = require('./routes/entrance');
+// const { registration, login } = require('./controllers/users');
+// const { registrationValidator, loginValidator } = require('./middlewares/validation');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const NotFoundError = require('./errors/NotFoundError');
 
 const app = express();
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_URL } = process.env;
 
-mongoose.connect('mongodb://localhost:27017/bitfilmsdb', { useNewUrlParser: true });
+mongoose.connect(DB_URL, { useNewUrlParser: true });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
-
-// app.post('/signin', loginValidator, login);
-// app.post('/signup', registrationValidator, registration);
-app.use('/', entranceRouter);
-
-app.use('/', userRouter);
-app.use('/', moviesRouter);
-
-app.use(errorLogger);
-app.use(errors());
-
-app.use((err, req, res) => {
-  const { statusCode = 500, message } = err;
-
-  res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
-});
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -42,5 +29,34 @@ const limiter = rateLimit({
 });
 
 app.use(limiter);
+
+// app.post('/signin', loginValidator, login);
+// app.post('/signup', registrationValidator, registration);
+app.use(entranceRouter);
+
+app.use('/', userRouter);
+app.use('/', moviesRouter);
+
+app.use('*', () => {
+  throw new NotFoundError('Запрашиваемый ресурс не найден');
+});
+
+app.use(errorLogger);
+app.use(errors());
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
+
+  next();
+});
+
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000,
+//   max: 100,
+// });
+
+// app.use(limiter);
 
 app.listen(PORT);
