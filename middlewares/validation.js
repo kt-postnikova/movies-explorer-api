e689864da746a864373a8fc2e335d6a04c383ab7
kt-postnikova <kt-postnikova@yandex.ru>
2021-09-11 13:03:09 +0300

    Исправлено: попытка создать пользователя без имени не прерывается celebrate и уходит контроллеру

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -28,7 +28,7 @@ const registrationValidator = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
   }),
 });
 
