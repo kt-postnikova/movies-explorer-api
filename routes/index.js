b1ab964fec31a7a575bb162950111cc384837fc5
kt-postnikova <kt-postnikova@yandex.ru>
2021-09-12 13:18:24 +0300

    Исправлено: маршрут по умолчанию не защищён авторизацией

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const indexRouter = require('express').Router();
 const NotFoundError = require('../errors/NotFoundError');
+const auth = require('../middlewares/auth');
 
-indexRouter.get('*', () => {
+indexRouter.get('*', auth, () => {
   throw new NotFoundError('Запрашиваемый ресурс не найден');
 });
 
