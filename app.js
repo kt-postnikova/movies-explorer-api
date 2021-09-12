1afc6bbf885521b0b1fc68a21d1ebb951198f9e5
kt-postnikova <kt-postnikova@yandex.ru>
2021-09-12 13:07:32 +0300

    Исправлено: в режиме разработки вместо строки с адресом передано undefined

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const NotFoundError = require('./errors/NotFoundError');
 
 const app = express();
 
-const { PORT = 3000, DB_URL } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
 
 mongoose.connect(DB_URL, { useNewUrlParser: true });
 
