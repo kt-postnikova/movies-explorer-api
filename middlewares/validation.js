026da2b6c4bd4f160193dec8710c305b1aaf5b0b
kt-postnikova <kt-postnikova@yandex.ru>
2021-10-12 18:24:11 +0300

    Добавлена валидация для обновления информации о пользователе

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -17,6 +17,13 @@ const userInfoValidator = celebrate({
   }),
 });
 
+const updateUserInfoValidator = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email(),
+    name: Joi.string().min(2).max(30),
+  }),
+});
+
 const loginValidator = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
@@ -50,6 +57,7 @@ const createMovieValidator = celebrate({
 
 module.exports = {
   userInfoValidator,
+  updateUserInfoValidator,
   loginValidator,
   registrationValidator,
   createMovieValidator,
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,10 @@
 const usersRouter = require('express').Router();
 const { getUserInfo, updateUserInfo } = require('../controllers/users');
-const { userInfoValidator } = require('../middlewares/validation');
+const { userInfoValidator, updateUserInfoValidator } = require('../middlewares/validation');
 const auth = require('../middlewares/auth');
 
 usersRouter.get('/users/me', auth, userInfoValidator, getUserInfo);
-usersRouter.patch('/users/me', auth, userInfoValidator, updateUserInfo);
+usersRouter.patch('/users/me', auth, updateUserInfoValidator, updateUserInfo);
 
 // usersRouter.delete('/users/:id', deleteUser);
 // usersRouter.get('/users', getUsers);
