dd52806411c59cf4975545293db15d36e45e8428
kt-postnikova <kt-postnikova@yandex.ru>
2021-11-17 11:36:17 +0300

    changes

--- routes/movie.js
+++ routes/movie.js
@@ -3,7 +3,7 @@ const { getMovies, createMovie, deleteMovie } = require('../controllers/movies')
 const auth = require('../middlewares/auth');
 const { createMovieValidator } = require('../middlewares/validation');
 
-moviesRouter.get('/movies', getMovies);
+moviesRouter.get('/movies', auth, getMovies);
 moviesRouter.post('/movies', auth, createMovieValidator, createMovie);
 moviesRouter.delete('/movies/:id', auth, deleteMovie);
 
