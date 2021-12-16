d6f2b7b9926833946b6c99bd49ac2547a7617ab5
kt-postnikova <kt-postnikova@yandex.ru>
2021-12-16 16:26:42 +0300

    Удален закомментированный код

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,6 +1,5 @@
 const Movie = require('../models/movie');
 const NotFoundError = require('../errors/NotFoundError');
-// const ForbiddenError = require('../errors/ForbiddenError');
 
 const getMovies = (req, res, next) => {
   const userId = req.user._id;
@@ -12,43 +11,12 @@ const getMovies = (req, res, next) => {
 };
 
 const createMovie = (req, res, next) => {
-  // const owner = req.user._id;
-  // const {
-  //   country,
-  //   director,
-  //   duration,
-  //   year,
-  //   description,
-  //   image,
-  //   trailer,
-  //   thumbnail,
-  //   nameRU,
-  //   nameEN,
-  //   movieId,
-  // } = req.body;
-
-  // Movie.create({
-  //   country,
-  //   director,
-  //   duration,
-  //   year,
-  //   description,
-  //   image,
-  //   trailer,
-  //   thumbnail,
-  //   nameRU,
-  //   nameEN,
-  //   movieId,
-  //   owner,
-  // })
   Movie.create({ ...req.body, owner: req.user._id })
     .then((movie) => res.send({ movie }))
     .catch(next);
 };
 
 const deleteMovie = (req, res, next) => {
-  // const ownerId = req.user._id;
-  // const { id } = req.params;
   Movie.find({ _id: req.params.id })
     .then((movie) => {
       if (!movie) {
@@ -57,12 +25,6 @@ const deleteMovie = (req, res, next) => {
       Movie.deleteOne({ _id: req.params.id })
         .then(() => res.send({ message: 'Фильм удалён' }))
         .catch(next);
-      // if (movie.owner.toString() !== ownerId) {
-      //   throw new ForbiddenError('Нет доступа к данным');
-      // }
-      // return movie.remove()
-      //   .then(() => res.send({ message: 'Фильм удалён' }))
-      //   .catch(next);
     })
     .catch(next);
 };
